aaec860c7e245d6507ff179dcca135a7eb2dc219
mannan-bitsclan <mannan.bitsclan@gmail.com>
2026-05-05 18:15:09 +0500

    latest code

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,6 +1,6 @@
 export const NAV_LINKS = [
-  { label: "Challenges", id: "section-problems" },
-  { label: "What We Offer", id: "section-arsenal" },
+  // { label: "Challenges", id: "section-problems" },
+  { label: "What We Offer", id: "section-problems-solutions" },
   { label: "The Process", id: "section-how" },
   { label: "Insights", id: "section-blog" },
   { label: "FAQ", id: "section-faq" },
